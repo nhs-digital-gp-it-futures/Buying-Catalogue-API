6559e15e9854c54f26885e60d51c06a7591cef20
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-07-31 09:18:12 +0100

    - removed link manager

--- NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Interfaces/Interfaces/ILinkManagerLogic.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NHSD.GPITF.BuyingCatalog.Interfaces
-{
-#pragma warning disable CS1591
-  public interface ILinkManagerLogic
-  {
-    void FrameworkSolutionCreate(string frameworkId, string solutionId);
-  }
-#pragma warning restore CS1591
-}
--- NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Logic/Interfaces/ILinkManagerValidator.cs
+++
@@ -1,8 +0,0 @@
-﻿using FluentValidation;
-
-namespace NHSD.GPITF.BuyingCatalog.Logic
-{
-  public interface ILinkManagerValidator : IValidatorBase<object>
-  {
-  }
-}
